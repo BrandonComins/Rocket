988db732f56d6e9b1e939312ad9b056eaa15cf01
BrandonComins <brandcomin@gmail.com>
2019-04-05 09:23:58 -0700

    remove gps and change to ADXL 337

--- Main.ino
+++ Main.ino
@@ -1,44 +1,34 @@
 #include "Arduino.h"
 #include "I2Cdev.h"
-#include "ADXL345.h"
 #include "SFE_BMP180.h"
 #include "SoftwareSerial.h"
-#include "Adafruit_GPS.h"
 #include "SD.h"
 
 
 
 // Pin Definitions
-#define GPSADAFUIT_PIN_RX  3
-#define GPSADAFUIT_PIN_TX 2
-#define SDFILE_PIN_CS 10
-
+  #define SDFILE_PIN_CS 10
 
 
 // Global variables and defines
+  
+  SFE_BMP180 altimeter;
 
-int16_t adxlAx, adxlAy, adxlAz;
-#define GPSECHO  true
-uint32_t timer = millis(); 
-SoftwareSerial gpsSerial(GPSADAFUIT_PIN_TX,GPSADAFUIT_PIN_RX);
-Adafruit_GPS gpsAdafuit(&gpsSerial);
-File sdFile;
-ADXL345 adxl;
-SFE_BMP180 bmp180;
+  int scale = 3;
+boolean micro_is_5V = true;
 
+  File sdFile;
 
-
-const int timeout = 10000;       //define timeout of 10 sec
-long time0;
+  double altimeterAltitude = altimeter.altitude();
+  double altimeterPressure = altimeter.getPressure();
+  double altimeterTempC = altimeter.getTemperatureC();
 
 void setup() 
 {
     Serial.begin(9600);
+ 
+    altimeter.begin();
     
-    adxl.init();
-
- bmp180.begin();
-    gpsAdafuit.init();
   
     pinMode(SDFILE_PIN_CS, OUTPUT);
     // Check if the card is present and can be initialized
@@ -50,71 +40,61 @@ void setup()
     
 }
 
-void loop()
-{
+void loop(){
+  //constantly declaring the value for the pins
+    int rawX = analogRead(A0);
+    int rawY = analogRead(A1);
+    int rawZ = analogRead(A2); 
+    float scaledX, scaledY, scaledZ;
+     
+  
+  
    sdFile = SD.open("datalog.txt", FILE_WRITE);
-    // if the file exists in SD card, write sensor data
+
     if (sdFile) {
-      writeData();
+      writeAltimeter();
+        
+        if (micro_is_5V) // Microcontroller runs off 5V
+  {
+    scaledX = mapf(rawX, 0, 675, -scale, scale); // 3.3/5 * 1023 =~ 675
+    scaledY = mapf(rawY, 0, 675, -scale, scale);
+    scaledZ = mapf(rawZ, 0, 675, -scale, scale);
+  }
+  else // Microcontroller runs off 3.3V
+  {
+    scaledX = mapf(rawX, 0, 1023, -scale, scale);
+    scaledY = mapf(rawY, 0, 1023, -scale, scale);
+    scaledZ = mapf(rawZ, 0, 1023, -scale, scale);
+  }
+      // Print out raw X,Y,Z accelerometer readings
+      sdFile.print(F("X: ")); sdFile.println(rawX);
+      sdFile.print(F("Y: ")); sdFile.println(rawY);
+      sdFile.print(F("Z: ")); sdFile.println(rawZ);
+      sdFile.println();
+  
+  // Print out scaled X,Y,Z accelerometer readings
+      sdFile.print(F("X: ")); sdFile.print(scaledX); sdFile.println(F(" g"));
+      sdFile.print(F("Y: ")); sdFile.print(scaledY); sdFile.println(F(" g"));
+      sdFile.print(F("Z: ")); sdFile.print(scaledZ); sdFile.println(F(" g"));
+      sdFile.println();
     }else{
         // if the file didn't open, print an error
         Serial.println(F("error opening file."));   
     }
-      if (timer > millis())  timer = millis();
 }
 
-    void writeData(){
-         
-    //Accelerometer
-      adxl.getAcceleration(&adxlAx, &adxlAy, &adxlAz);
-      
-      sdFile.println(F("Accelerometer Data")); 
-      sdFile.println(("Accelerometer X: " + adxlAx)); 
-      sdFile.println(("Accelerometer Y: " + adxlAy));   
-      sdFile.println(("Acceleromete Z: " + adxlAz));
 
-    //Altimeter
-      double bmp180Alt = bmp180.altitude();
-      double bmp180Pressure = bmp180.getPressure();
-      double bmp180TempC = bmp180.getTemperatureC();
-
-        
-      sdFile.print(bmp180Alt,1); sdFile.print(F(" [m]"));
-      sdFile.print(bmp180Pressure,1); sdFile.print(F(" [hPa]"));
-      sdFile.print(bmp180TempC,1); sdFile.println(F(" [°C]"));
-
-      //GPS
-    
-      gpsSerial.println(F("Altimeter Data"));
-      gpsSerial.println(F("Altitude: "));
-      gpsSerial.println(F("\tpressure: ")); 
-      gpsSerial.println(F("\tTemperature: ")); 
-
-
-      if (millis() - timer > 2000) {
-        timer = millis(); // reset the timer
-
-        gpsSerial.println(F("\nTime: "));
-        gpsSerial.println(gpsAdafuit.hour, DEC); gpsSerial.print(':');
-        gpsSerial.println(gpsAdafuit.minute, DEC); gpsSerial.print(':');
-        gpsSerial.println(gpsAdafuit.seconds, DEC); gpsSerial.print('.');
-        gpsSerial.println(gpsAdafuit.milliseconds);
-        gpsSerial.println(F(" quality: ")); gpsSerial.println((int)gpsAdafuit.fixquality);
-        if (gpsAdafuit.fix) {
-          gpsSerial.println(F("Location: "));
-          gpsSerial.println(gpsAdafuit.latitude, 4); gpsSerial.print(gpsAdafuit.lat);
-          gpsSerial.println(gpsAdafuit.longitude, 4); gpsSerial.println(gpsAdafuit.lon);
-          gpsSerial.println("Location (in degrees, works with Google Maps): ");
-          gpsSerial.println(gpsAdafuit.latitudeDegrees, 4);
-          gpsSerial.println(gpsAdafuit.longitudeDegrees, 4);
-
-          gpsSerial.println("Speed (knots): "); gpsSerial.println(gpsAdafuit.speed);
-          gpsSerial.println("Angle: "); gpsSerial.println(gpsAdafuit.angle);
-          gpsSerial.println("Altitude: "); gpsSerial.println(gpsAdafuit.altitude);
-          gpsSerial.println("Satellites: "); gpsSerial.println((int)gpsAdafuit.satellites);
-        }
-      }
+    void writeAltimeter(){
+      sdFile.println(altimeterAltitude,1); sdFile.print(F(" [m]"));
+      sdFile.println(altimeterPressure,1); sdFile.print(F(" [hPa]"));
+      sdFile.println(altimeterTempC,1); sdFile.println(F(" [°C]"));
     }
+
+    float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+  {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
 
 
  
